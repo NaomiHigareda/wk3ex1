a3763bf52a5740553abe31f570a51ea1e1b9b9bc
Naomi Higareda <nfhigareda@actx.edu>
2025-02-04 12:33:36 -0600

    method

--- wk3ex1/Program.cs
+++ wk3ex1/Program.cs
@@ -9,33 +9,56 @@ namespace wk3ex1
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static double ConvertToUSD(double amount, string fromCurrency)
         {
-            //prompt user to enter a currency
-            Console.WriteLine("Enter an amount");
-            //Convert currency to usd
-            double amount = Convert.ToInt32(Console.ReadLine());
-            //prompt user to enter a currency to convert in to usd
-            Console.WriteLine("Enter an amount to convert into usd");
-            string fromCurrency = Console.ReadLine();
-            //prompt user to enter a currency to convert in to eur
-            Console.WriteLine("Enter an amount to convert into eur");
-            //convert currency to eur
-            string toCurrency = Console.ReadLine();
-            //prompt user to enter a currency to convert into JPY
-            Console.WriteLine ("Enter a currency to convert into JPY");
-            //convert currency to jpy
-            string currency = Convert.ToString(Console.ReadLine());
-            
-            double convertedAmount = 0;
+            double returnValue = 0;
+
+            if (fromCurrency == "EUR")
             {
-                
+                returnValue = amount * 0.9;
+            }
+            else if (fromCurrency == "JPY") 
+            {
+                returnValue = amount * 20;
             }
 
-            
+            return returnValue;
+        }
 
+        static double ConvertToEUR(double amount, string fromCurrency)
+        {
+            double returnValue = 0;
 
-            
+            if (fromCurrency == "USD")
+            {
+                returnValue = amount * 0.9;
+            }
+            else if (fromCurrency == "JPY")
+            {
+                returnValue = amount * 25;
+            }
+
+            return returnValue;
+        }
+        static double ConvertToJPY(double amount, string fromCurrency)
+        {
+            double returnValue = 0;
+
+            if (fromCurrency == "USD")
+            {
+                returnValue = amount * 1.5;
+            }
+            else if (fromCurrency == "EUR")
+            {
+                returnValue = amount * 30;
+            }
+
+            return returnValue;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1 dollars equals to " + ConvertToEUR(1, "USD") + " EUR");
+            Console.WriteLine("1 JPY equals to " + ConvertToEUR(1, "JPY") + " JPY");
         }
     }
 }
